6baaf9eb7bbd461eba16f09a57fd91a740c3503b
ksenla <159244875+ksenla@users.noreply.github.com>
2024-02-28 12:22:46 +0300

    Delete lab3.1.py

--- lab3.1.py
+++
@@ -1,9 +0,0 @@
-N = int(input("Введите количество слов: "))
-result = ""
-
-for i in range(N):
-    word = input(f"Введите слово {i+1}: ")
-    result += word + " "
-
-print("Результат соединения слов:")
-print(result.strip())
