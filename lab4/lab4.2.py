33517084df40f95bf7fd1215c09aa077a32c16e0
ksenla <kseniagusevva@yandex.ru>
2024-03-20 12:22:09 +0300

    2

--- lab4/lab4.2.py
+++ lab4/lab4.2.py
@@ -2,6 +2,6 @@
 chislo = int(input("введите число:"))
 if chislo != 0:
     res = 300 / chislo
-    print("300/" chislo "=" res)
+    print("300/", chislo, "=", res)
 else:
     print("Ошибка. На ноль делить нельзя.")
