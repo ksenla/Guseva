0d781a272b1d32bb14517a989b7c64654feafdcf
ksenla <kseniagusevva@yandex.ru>
2024-03-27 13:07:39 +0300

    5

--- lab5.py
+++ lab5.py
@@ -3,11 +3,11 @@ def z1():
     set = [22, 25, 66, 7, 5]
     if chislo in set:
         print ("Поздравляю, Вы угадали число!")
-        else:
+    else:
         print ("Нет такого числа!")
     print ("Исходный список:", set)
     print ("Ваше число:", chislo)
-print(z1())
+#print(z1())
 
 def z2():
     list = [1, 22, 23, 22, 25, 76, 25]
@@ -15,6 +15,35 @@ def z2():
         if list.count(item) > 1 :
             print ("Повторяющиеся элементы:",item)
             break
-        else:
-            print ("Повторяющихся элементов нет.")
+    else:
+        print ("Повторяющихся элементов нет.")
 #print(z2())
+
+def z3():
+    week = ("Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье")
+    weekendnumber = int(input("Сколько выходных на неделе Вы хотите?"))
+    weekend = week[-weekendnumber:]
+    work = week[:-weekendnumber]
+    print("Ваши выходные дни:", ",".join(weekend))
+    print("Ваши рабочие дни:", ",".join(work))
+#print(z3())
+
+def z4():
+    md25 = ["Гусева", "Овчинников", "Зайцев", "Постников", "Токарева", "Поспелова", "Свириденко", "Петров", "Зотова", "Идрисова"]
+    md20 = ["Белкина", "Близгарёв", "Богданчук", "Казарян", "Кучев", "Кучинова", "Левченко", "Малкиель", "Решетников", "Семёнов"]
+    sportteam = tuple(md25[:5]+md20[:5])
+    print("МД-25:", md20)
+    print("МД-20:", md20)
+    print("Спортивная команда:", sportteam)
+    print ("Длина кортежа:", len(sportteam))
+
+    sortedteam = tuple(sorted(sportteam))
+    print("Сортированный кортеж:", sortedteam)
+
+    surname = "Иванов"
+    if surname in sportteam:
+        print("Фамилия Иванов входит в спортивную команду.")
+        print(f"Количество Ивановых:{sportteam.count(surname)}")
+    else:
+        print("Фамилия Иванов не входит в спортивную команду.")
+print (z4())
