5507fee1c340a048babe9194963b84bc6611825e
ksenla <kseniagusevva@yandex.ru>
2024-03-27 11:56:20 +0300

    4

--- lab4/lab4.4.py
+++ lab4/lab4.4.py
@@ -1,7 +1,8 @@
 bilet = input("Введите номер билета:")
 def lucky(bilet):
-    if len(bilet) % 2 != 0:
+    if len(bilet) % 2 != 0 or len(bilet)== 0:
         print ("введите корректный номер билета")
+        return False
     half = len(bilet) // 2
     return sum(map(int, bilet[:half])) == sum(map(int, bilet[half:]))
 if lucky(bilet):
