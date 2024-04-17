444cfdbf17ee596d2d9f4453367c4d9612af168e
ksenla <kseniagusevva@yandex.ru>
2024-04-17 13:42:28 +0300

    7

--- lab7.py
+++ lab7.py
@@ -6,11 +6,23 @@ def z1():
         print(f"Размер изображения:{img.size}")
         print(f"Формат изображения:{img.format}")
         print(f"Цветовая модель изображения:{img.mode}")
-#print(z1())
+z1()
 def z2():
     from PIL import Image
     filename = "buildings.jpg"
     with Image.open(filename) as img:
-        low_res_img = img.resize(img.width // 3, img.height // 3)
-        low_res_img.show()
-print(z2())
+        low_res_img = img.reduce(3)
+        img2 = img.transpose(Image.FLIP_LEFT_RIGHT)
+        img3 = img.transpose(Image.FLIP_TOP_BOTTOM)
+        low_res_img.save("first.jpg")
+        img2.save("second.jpg")
+        img3.save("third.jpg")
+z2()
+def z3():
+    from PIL import Image, ImageFilter
+    images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"]
+    for img in images:
+        image = Image.open(img)
+        imgfilt = image.filter(ImageFilter.CONTOUR)
+        imgfilt.save('filters/'+str(img))
+z3()
